5f17d2215891818473a19b44494e4a9562c62bd1
MIguel Lampreia <miguel_lampreia@eticalgarve.com>
2022-10-24 12:28:37 +0100

    Hide Hand When Grabbing Script

--- A Dangerous Mind/Assets/Textures/Scripts/VR/Grab_Restrictor.cs
+++ A Dangerous Mind/Assets/Textures/Scripts/VR/Grab_Restrictor.cs
@@ -6,24 +6,44 @@ public class Grab_Restrictor : MonoBehaviour
 {
     [SerializeField] private GameObject rightHandRay;
     [SerializeField] private GameObject leftHandRay;
-
-    public void DisableRightRay()
+    [SerializeField] private GameObject rightHand;
+    [SerializeField] private GameObject leftHand;
+    [SerializeField] private SkinnedMeshRenderer rightHandMesh;
+    [SerializeField] private SkinnedMeshRenderer leftHandMesh;
+    public void RightHandGrabON()
     {
+        Color color = rightHandMesh.material.color;
+        color.a = 0;
+        rightHandMesh.material.color = color;
+
         rightHandRay.SetActive(false);
+        
     }
 
-    public void DisableLeftRay()
+    public void LeftHandGrabOn()
     {
+        Color color = leftHandMesh.material.color;
+        color.a = 0;
+        rightHandMesh.material.color = color;
+
         leftHandRay.SetActive(false);
     }
 
-    public void EnableRightRay()
+    public void RightHandGrabOff()
     {
+        Color color = rightHandMesh.material.color;
+        color.a = 1;
+        rightHandMesh.material.color = color;
+
         rightHandRay.SetActive(true);
     }
 
-    public void EnableLeftRay()
+    public void LeftHandGrabOff()
     {
+        Color color = leftHandMesh.material.color;
+        color.a = 1;
+        rightHandMesh.material.color = color;
+
         leftHandRay.SetActive(true);
     }
 }
