f03a6602c82f6ce079c54c6d2319574218cad704
Miguel Lampreia Home <miguellampreia0@eticalgarve.com>
2023-02-23 18:16:17 +0000

    Fixed Fake Hand and Decal

--- A Dangerous Mind/Assets/Scripts/FakeHandMover.cs
+++ A Dangerous Mind/Assets/Scripts/FakeHandMover.cs
@@ -8,23 +8,52 @@ public class FakeHandMover : MonoBehaviour
     [SerializeField] private Transform pivot;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Vector3 dir;
-
+    float timer;
+    bool ready;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        dir = pivot.position - transform.position;
-        if (dir.x < 0.01f && dir.x > -0.01f && dir.y < 0.01f && dir.y > -0.01f && dir.z < 0.01f && dir.z > -0.01f)
+        if (timer > 2 && !ready)
         {
-
+            ready = true;
         }
         else
         {
-           rb.MovePosition(transform.position + dir * Time.deltaTime * 12);
+            timer += Time.deltaTime;
         }
-        rb.MoveRotation(pivot.rotation);
+    }
+
+    private void FixedUpdate()
+    {
+        if (ready)
+        {
+            dir = pivot.position - transform.position;
+            if (dir.x < 0.01f && dir.x > -0.01f && dir.y < 0.01f && dir.y > -0.01f && dir.z < 0.01f && dir.z > -0.01f)
+            {
+
+            }
+            else
+            {
+               rb.MovePosition(transform.position + dir * Time.deltaTime * 2);
+            }
+            rb.MoveRotation(pivot.rotation);
+        }
+        else
+        {
+            dir = pivot.position - transform.position;
+            if (dir.x < 0.01f && dir.x > -0.01f && dir.y < 0.01f && dir.y > -0.01f && dir.z < 0.01f && dir.z > -0.01f)
+            {
+
+            }
+            else
+            {
+                rb.MovePosition(transform.position + dir * Time.deltaTime);
+            }
+            rb.MoveRotation(pivot.rotation);
+        }
     }
 }
