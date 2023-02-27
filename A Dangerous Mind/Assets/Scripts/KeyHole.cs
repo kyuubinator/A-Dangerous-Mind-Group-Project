2f5e781a67ac30e1c98c1ae7e8f92b36185bd04c
MIguel Lampreia <miguel_lampreia@eticalgarve.com>
2023-02-27 12:41:46 +0000

    Fridge Script Organized the scripts folder Applied door script to Bedroom door (i think...)

--- A Dangerous Mind/Assets/Scripts/KeyHole.cs
+++ A Dangerous Mind/Assets/Scripts/KeyHole.cs
@@ -1,31 +1,31 @@
+using Newtonsoft.Json.Serialization;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Transactions.Configuration;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyHole : MonoBehaviour
 {
-    [SerializeField] private GameObject handle;
-    [SerializeField] private GameObject key;
-    [SerializeField] private GameObject block;
+    [SerializeField] private XRGrabInteractable interactable;
+
+    private void Start()
+    {
+         interactable = GetComponentInParent<XRGrabInteractable>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Key")
+        if (other.gameObject.CompareTag("Key"))
         {
             other.gameObject.SetActive(false);
-            if (key != null)
+            if (interactable != null)
             {
-                key.SetActive(true);
+                interactable.enabled = true;
             }
-            if (handle != null)
-            {
-                handle.SetActive(true);
-            }
-            if (block != null)
-            {
-                block.SetActive(false);
-            }
+            Destroy(this);
         }
     }
 }
